2dff3f0c90c557f0e921405b079a35583cdab9a8
AndradeMatheus <matheus.andrade.santos@gmail.com>
2020-06-17 23:40:31 -0300

    Corrigido conexão com o banco

--- src/br/com/fatec/util/ConexaoDB.java
+++ src/br/com/fatec/util/ConexaoDB.java
@@ -1,25 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.fatec.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author ProfAlexandre
- */
 public class ConexaoDB {
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String url ="jdbc:mysql://localhost:3306/db2020";
             String usuario = "root";
-            String senha = "admin";
+            String senha = "password";
             return DriverManager.getConnection(url,usuario,senha);
         } catch (SQLException e) {
             throw new RuntimeException(e);
