868e6b309ac42a17399b8f32dd6d08a2a916a621
AndradeMatheus <matheus.andrade.santos@gmail.com>
2020-06-19 19:11:57 -0300

    Adicionado JUnit tests

---
+++ test/br/com/fatec/controler/ControleUsuarioTest.java
@@ -0,0 +1,39 @@
+package br.com.fatec.controler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControleUsuarioTest {
+
+	@Test
+	void testBuscarUsuarioPorId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAlterarUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExcluirUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidaLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListarUsuarios() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInserirUsuario() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/br/com/fatec/controler/ControleVacinaTest.java
@@ -0,0 +1,34 @@
+package br.com.fatec.controler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControleVacinaTest {
+
+	@Test
+	void testBuscarVacinaPorId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAlterarVacina() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExcluirVacina() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListarVacinas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInserirVacina() {
+		fail("Not yet implemented");
+	}
+
+}
