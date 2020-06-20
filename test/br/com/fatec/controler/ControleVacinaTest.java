72c42603fd753ca33a7521999ffaa77893f3cc1e
AndradeMatheus <matheus.andrade.santos@gmail.com>
2020-06-19 22:17:44 -0300

    Criado testes de Controller para Vacina

--- test/br/com/fatec/controler/ControleVacinaTest.java
+++ test/br/com/fatec/controler/ControleVacinaTest.java
@@ -1,34 +1,75 @@
 package br.com.fatec.controler;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
+import br.com.fatec.bean.Vacina;
+import java.util.List;
 
 class ControleVacinaTest {
 
-	@Test
-	void testBuscarVacinaPorId() {
-		fail("Not yet implemented");
+	@@Test
+	void testInsertVacina() throws ClassNotFoundException, SQLException {
+		Vacina vac = new Vacina(0, "Teste", 3);
+		
+		ControleVacina vacController = new ControleVacina();
+		Boolean valida = vacController.InserirVacina(vac);
+		Vacina vacNova = vacController.ListarVacinas().get(vacController.ListarVacinas().size() -1);
+
+		if(valida)
+			assertEquals(vac.GetNome(), vacNova.GetNome());
+		else
+			assertTrue(false);
 	}
 
 	@Test
-	void testAlterarVacina() {
-		fail("Not yet implemented");
+	void testListarVacinas() throws ClassNotFoundException, SQLException {
+		ControleVacina vacController = new ControleVacina();
+		List<Vacina> vacs = vacController.ListarVacinas();
+
+		assertNotNull(vacs);
 	}
 
 	@Test
-	void testExcluirVacina() {
-		fail("Not yet implemented");
+	void testBuscarVacinaPorId() throws ClassNotFoundException, SQLException {
+		ControleVacina vacController = new ControleVacina();
+		Vacina vac = vacController.BuscarVacinaPorId(new Vacina(1, "", 0));
+
+		Vacina vacComparison = vacController.ListarVacinas().get(1);
+
+		assertEquals(vac.GetNome(), vacComparison.GetNome());
 	}
 
 	@Test
-	void testListarVacinas() {
-		fail("Not yet implemented");
+	void testAlterarVacina() throws ClassNotFoundException, SQLException{
+		ControleVacina vacController = new ControleVacina();
+
+		Vacina vac = new Vacina();
+		vac = vacController.ListarVacinas().get(vacController.ListarVacinas().size() -1);
+
+		Vacina vacModificado = new Vacina();
+		vacModificado.SetNome("TESTEALTERACAO");
+
+		Boolean valida = vacController.AlterarVacina(vacModificado, vac);
+
+		if(valida)
+			assertEquals("TESTEALTERACAO", vacController.ListarVacinas().get(vacController.ListarVacinas().size() -1);
+		else
+			assertTrue(false);
 	}
 
 	@Test
-	void testInserirVacina() {
-		fail("Not yet implemented");
-	}
+	void testExcluir() throws ClassNotFoundException, SQLException{
+		ControleVacina vacController = new ControleVacina();
+		
+		Vacina ultimaVacina = new Vacina();
+		Vacina vac = vacController.ListarVacinas().get(vacController.ListarVacinas().size() -1);
 
+		Boolean validaExclusao = vacController.ExcluirVacina(vac);
+
+		if(validaExclusao)
+			ultimaVacina = vacController.ListarVacinas().get(vacController.ListarVacinas().size() -1);
+
+		assertNotEquals(ultimaVacina.GetNome(), vac.GetNome());
+	}
 }
